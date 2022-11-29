9fee569afa106f0c4f724a06ed434a5f85612411
pecotina <vbianchinotti@fcien.edu.uy>
2022-11-29 10:06:49 -0300

    codigo para cargar los datos

---
+++ 02_codigos/cargando_datos.R
@@ -0,0 +1,34 @@
+# !diagnostics off
+library(tidyverse)
+library(janitor)
+
+# cargo la planilla de datos con todos los registros
+datos <- data_frame(read_csv("01_datos/rendimientos_2015_2019.csv", 
+                            col_types = cols(Aprobado = col_factor(levels = c()), 
+                                             `Código materia` = col_factor(levels = c()), 
+                                             Materia = col_factor(levels = c()), 
+                                             Servicio = col_factor(levels = c()), 
+                                             `Servicio instancia` = col_factor(levels = c()), 
+                                             Sexo = col_factor(levels = c()), 
+                                             `Tipo actividad` = col_factor(levels = c())))) %>% 
+  clean_names()
+
+# View(problems(datos))
+# 
+# View(datos[338, ])
+
+
+# filtrado por materias curriculares --------------------------------------
+
+datos <- datos %>%
+  dplyr::select(servicio, 
+                codigo_materia, 
+                materia, 
+                aprobado, 
+                nota, 
+                fecha,
+                servicio_instancia) %>% 
+  filter(str_detect(codigo_materia, "98")) %>% 
+  filter(codigo_materia != "1098")
+
+
