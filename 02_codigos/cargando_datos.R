800c610ab74bbf5d122fcaa2d1201957da38d407
PecoTina <vbianchinotti@fcien.edu.uy>
2022-12-05 07:27:20 -0300

    agregando la seleccion de servicio y materias

--- 02_codigos/cargando_datos.R
+++ 02_codigos/cargando_datos.R
@@ -13,13 +13,9 @@ datos_bed <- data_frame(read_csv("01_datos/rendimientos_2015_2019.csv",
                                              `Tipo actividad` = col_factor(levels = c())))) %>% 
   clean_names()
 
-# View(problems(datos))
-# 
-# View(datos[338, ])
-
-
 # filtrado por materias curriculares --------------------------------------
 
+# selecciono los cursos obligatorios del tronco comun
 datos_bed <- datos_bed %>%
   dplyr::select(servicio, 
                 codigo_materia, 
@@ -33,4 +29,26 @@ datos_bed <- datos_bed %>%
   filter(materia != "INGLES TECNICO (OPCIONAL)") %>% 
   filter(materia != "INGLES TECNICO II (OPCIONAL)")
 
+# seleccionando datos montevideo ------------------------------------------
+
+# selecciono los datos de montevideo
+# separo fecha en tres variables para quedarmo solo con el ano
+datos <- datos_bed %>% 
+  filter(servicio_instancia == "FVET") %>% 
+  separate(fecha, c("dia", "periodo", "anho")) %>% 
+  mutate(dia = as.factor(dia),
+         periodo = as.factor(periodo),
+         ano = as.factor(anho))
+
+
+datos <- datos %>% 
+  mutate(aprobado = as.character(aprobado))
+
+# creo el nivel exonerado
+datos$aprobado[datos$nota > 0] <- "Exonerado"
+
+# renoimbro la variable aprobado como resultado
+datos <- datos %>% 
+  mutate(aprobado = as.factor(aprobado)) %>% 
+  rename(resultado = aprobado)
 
